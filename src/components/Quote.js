7cc6c7560192f76e4c74dac942758334d84d76db
valentina vittoria <valeviit@icloud.com>
2023-02-18 23:05:13 +0100

    edit some css

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,8 +4,8 @@ const Quote = ({quote}) => {
     
     return (
     <div className='quote-container'>
-    <p>{quote.text}</p>
-      <p>{quote.author}</p>
+    <p className='quote'>{quote.text}</p>
+      <p className='author'><em>{quote.author}</em></p>
         </div>
     );
 }
