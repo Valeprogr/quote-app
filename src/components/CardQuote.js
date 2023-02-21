73acdf8663812f31d3a29bf50cd6f2032bad2b89
valentina vittoria <valeviit@icloud.com>
2023-02-21 19:38:36 +0100

    add delete function and btn

--- src/components/CardQuote.js
+++ src/components/CardQuote.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
 const CardQuote = ({index,ele}) => {
+    
+    //Delete Quote function
+    const deleteItem = () =>{
+        console.log(index)
+       let arrList = JSON.parse(localStorage.getItem('list'))
+        arrList.splice(index,1);
+        console.log(arrList)
+      localStorage.setItem("list", JSON.stringify(arrList));
+    
+    }
     return (
-        <div className='card-quote' index={index}>
+       
+            <div className='card-quote' index={index}>
+            <div className='icon'>
+            <span className="material-symbols-outlined close" onClick={deleteItem} >
+                close
+            </span>
+            </div>
             <p>{ele.text}</p>
             <p>{ele.author}</p>
         </div>
